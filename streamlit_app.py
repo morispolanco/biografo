a6d1cee507d3b18010bae5806522fa2ab1445789
morispolanco <morispolanco@gmail.com>
2024-02-01 03:35:39 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,45 +2,49 @@ import streamlit as st
 import requests
 import json
 
-# Función para llamar a la API y corregir el texto
+# Función para corregir texto utilizando la API
 def corregir_texto(texto):
-    response = requests.post(
-        "https://api.respell.ai/v1/run",
-        headers={
-            # This is your API key
-            "Authorization": "Bearer 260cee54-6d54-48ba-92e8-bf641b5f4805",
-            "Accept": "application/json",
-            "Content-Type": "application/json"
-        },
-        data=json.dumps({
-            "spellId": "EvBQVBWF4tRxbbJFc3ULR",
-            # This field can be omitted to run the latest published version
-            "spellVersionId": "qz5t6E9DnzKAsW-9UiHtU",
-            # Fill in a value for your dynamic input block
-            "inputs": {
-                "input": texto,
-            }
-        }),
-    )
+    # URL de la API
+    url = "https://api.respell.ai/v1/run"
     
+    # Cuerpo de la solicitud
+    payload = {
+        "spellId": "dN5cL9gF7TOXGpQHIxkeb",
+        "spellVersionId": "QvIqkK5I7fhzvQfqJ6oOr",
+        "inputs": {
+            "input": texto,
+        }
+    }
+
+    # Cabeceras de la solicitud
+    headers = {
+        "Authorization": "Bearer 260cee54-6d54-48ba-92e8-bf641b5f4805",
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+    }
+
+    # Realizar la solicitud POST
+    response = requests.post(url, headers=headers, data=json.dumps(payload))
+
+    # Verificar si la solicitud fue exitosa
     if response.status_code == 200:
         data = response.json()
-        corrected_text = data.get("outputs", {}).get("output", "")
-        return corrected_text
+        texto_corregido = data["outputs"]["output"]
+        return texto_corregido
     else:
-        return "Error al procesar el texto. Por favor, inténtalo de nuevo más tarde."
+        return "Error al corregir el texto. Por favor, inténtalo de nuevo más tarde."
 
-# Configuración de la aplicación de Streamlit
-st.title("Corrección de Texto con respell.ai")
+# Configuración de la aplicación Streamlit
+st.title("Corrección de Texto")
 
 # Área de entrada de texto
-texto_input = st.text_area("Introduce el texto que quieres corregir:")
+texto_usuario = st.text_area("Ingresa el texto que quieres corregir")
 
 # Botón para corregir el texto
-if st.button("Corregir Texto"):
-    if texto_input:
-        texto_corregido = corregir_texto(texto_input)
-        st.subheader("Texto Corregido:")
+if st.button("Corregir"):
+    if texto_usuario:
+        texto_corregido = corregir_texto(texto_usuario)
+        st.subheader("Texto Corregido")
         st.write(texto_corregido)
     else:
-        st.warning("Por favor, introduce texto para corregir.")
+        st.warning("Por favor, ingresa texto para corregir")
